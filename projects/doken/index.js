f65b919dc6eaaa64d2cf7e6b05e64225a1db5e4a
doken1 <102377300+doken1@users.noreply.github.com>
2022-03-25 22:13:05 +0530

    Update index.js

--- projects/doken/index.js
+++ projects/doken/index.js
@@ -1,31 +1,72 @@
 const sdk = require('@defillama/sdk');
-const { transformBscAddress } = require('../helper/portedTokens');
-const DOKEN_TOKEN_CONTRACT = '0xf9a2d40589271be17612a3f57a9028a568f56e3d';
-const DOKEN_BOND_CONTRACT = '';
+const abi = require("./abi.json");
+const { addTokensAndLPs } = require("../helper/unwrapLPs");
+const erc20 = require("../helper/abis/erc20.json");
 
-async function tvl(timestamp, block, chainBlocks) {
+const CHEFS = {
+  "bsc": "0xf9a2d40589271be17612a3f57a9028a568f56e3d"
+}
+
+async function chainTvl(timestamp, block, chainBlocks, chain) {
+  const chef = CHEFS[chain];
+  const poolLength = Number(
+    (
+      await sdk.api.abi.call({
+        abi: abi.poolLength,
+        target: chef,
+        chain: chain,
+        block: chainBlocks[chain],
+      })
+    ).output
+  );
+  const poolIds = Array.from(Array(poolLength).keys());
+
+  const lpTokens = (
+    await sdk.api.abi.multiCall({
+      abi: abi.poolInfo,
+      calls: poolIds.map((pid) => ({
+        target: chef,
+        params: pid,
+      })),
+      chain: chain,
+      block: chainBlocks[chain],
+    })
+  ).output.map((lp) => ({ output: lp.output[0].toLowerCase() }));
+
+  const amounts = (
+    await sdk.api.abi.multiCall({
+      abi: erc20.balanceOf,
+      calls: lpTokens.map((lp) => ({
+        target: lp.output,
+        params: chef,
+      })),
+      chain: chain,
+      block: chainBlocks[chain],
+    })
+  )
+
   const balances = {};
-  const transform = await transformBscAddress();
-
-  const collateralBalance = (await sdk.api.abi.call({
-    abi: 'erc20:balanceOf',
-    chain: 'bsc',
-    target: DOKEN_TOKEN_CONTRACT,
-    params: [DOKEN_BOND_CONTRACT],
-    block: chainBlocks['bsc'],
-  })).output;
-
-  await sdk.util.sumSingleBalance(balances, transform(DOKEN_TOKEN_CONTRACT), collateralBalance)
+  const tokens = { output: lpTokens };
+  const transformAddress = addr => `${chain}:${addr}`;
+  await addTokensAndLPs(
+    balances,
+    tokens,
+    amounts,
+    chainBlocks[chain],
+    chain,
+    transformAddress
+  );
 
   return balances;
 }
 
+async function bscTvl(timestamp, block, chainBlocks) {
+  return await chainTvl(timestamp, block, chainBlocks, "bsc")
+}
+
 module.exports = {
-  timetravel: true,
-  misrepresentedTokens: false,
-  methodology: 'counts the number of DOKEN tokens in the Club Bonding contract.'
-  start: 1000235,
+  methodology: "TVL includes all farms in MasterChef contract",
   bsc: {
-    tvl,
-  }
-}; 
+    tvl: bscTvl,
+  },
+}
